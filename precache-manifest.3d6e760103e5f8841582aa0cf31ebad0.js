c9df00408b1061350d1d065fcf30ba564a40c5b6
mar1an4ik <69079587+mar1an4ik@users.noreply.github.com>
2020-08-01 17:27:12 +0300

    Updates

---
+++ precache-manifest.3d6e760103e5f8841582aa0cf31ebad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9970636ca81cb30a5b78a28f2375aaa5",
+    "url": "/adds/index.html"
+  },
+  {
+    "revision": "b67faf049737d84edebd",
+    "url": "/adds/static/css/main.b2d68517.chunk.css"
+  },
+  {
+    "revision": "87b8c700eb65391bc5bc",
+    "url": "/adds/static/js/2.cc73df4b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adds/static/js/2.cc73df4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67faf049737d84edebd",
+    "url": "/adds/static/js/main.4e3a8bfc.chunk.js"
+  },
+  {
+    "revision": "0c9af0c85eeac1ae801b",
+    "url": "/adds/static/js/runtime-main.d00e5c41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/adds/static/media/logo.5d5d9eef.svg"
+  }
+]);
