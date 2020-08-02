6ddc399a4fcdce66ee997195a437262ebecb5d9a
mar1an4ik <69079587+mar1an4ik@users.noreply.github.com>
2020-08-02 12:33:41 +0300

    Add files via upload

---
+++ src/CreatePostForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import style from "./PostAlone.module.css"
+
+export const CreatePostForm=(props)=>{
+    let refTitle=React.createRef();
+    let refDescr=React.createRef();
+
+    const submit=()=>{
+        let nowDate = new Date();
+        let nowDateString=""+nowDate;
+        nowDateString=nowDateString.substring(0, 21);
+        props.addPost(refTitle.current.value,refDescr.current.value,nowDateString)
+        refDescr.current.value="";
+        refTitle.current.value="";
+    };
+
+
+
+    return (<div>
+
+
+
+    <div className={style.mainForm} >
+
+        <br/>
+        <div className={style.header2}>Create your add free!</div>
+        <div>
+        <textarea  ref={refTitle} rows={1} cols={50} placeholder={"Enter title"}/>
+        </div>
+        <div>
+        <textarea  ref={refDescr} className={style.textArea} placeholder={"Enter Description"} />
+        </div>
+        <div>
+        <button className={style.but}  onClick={submit}>Post</button>
+        </div>
+
+
+
+    </div>
+
+
+    </div>)
+
+};
